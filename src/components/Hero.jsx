a2c8fad81d6bc7826879cc1a8f5a6d95eb2a0172
ariesdimasy <ariesdimasy@gmail.com>
2023-11-20 21:26:48 +0700

    change to modular css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
-import "./Hero.css"
+import style from "./Hero.module.css"
+// import "./Hero.module.css"
 import Button from "./atoms/Button"
 
 export default function Hero() { 
-    return (<section className="hero">
+    return (<section className={`${style['hero-section']} ${style['apa']}`}>
         <div> This is hero </div>
         <Button type="button-success" >Click Me</Button>
         <Button type="button-danger" >Click Me</Button>
